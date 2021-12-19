f721feb4bb1f789b94fa41ffbc538433281a1258
Jenny Quach <jennyquach.99@hotmail.com>
2021-12-19 16:44:08 -0500

    Breaking down ui components into smaller peices

--- src/release_notice_board/core.cljs
+++ src/release_notice_board/core.cljs
@@ -65,42 +65,54 @@
    [repo-searchbar]
    [repo-suggestions]])
 
+(defn repo-item-toolbar [repo-full-name unread?]
+  [space/space {:direction :vertical :align :center}
+   [button/button {:shape    :circle
+                   :size     :small
+                   :on-click #(rf/dispatch [:repos/unwatch-repo repo-full-name])}
+    "-"]
+   (if unread?
+     [notification-filled/notification-filled
+      {:style    {:color :orange}
+       :on-click #(rf/dispatch [:repos/mark-read repo-full-name])}]
+     [notification-outlined/notification-outlined
+      {:style    {:color :grey}
+       :on-click #(rf/dispatch [:repos/mark-unread repo-full-name])}])])
 
+(defn repo-item-summary
+  [{repo-full-name :full_name
+    :keys [description html_url language watchers tag_name published_at]}]
+  [:div
+   [:a {:href html_url} repo-full-name]
+   [:p.repo__description description]
+   [row/row {:class "repo__other-details"}
+    [col/col {:xs 12 :lg 6} "language: " language]
+    [col/col {:xs 12 :lg 6} "watchers: " watchers]
+    [col/col {:xs 12 :lg 6} "latest-release: " tag_name]
+    [col/col {:xs 12 :lg 6} "published: " published_at]]])
 
-(defn repo-watching-section []
+(defn watched-repo-item
+  [{repo-full-name :full_name 
+    :keys [last-seen] 
+    :as   repo}]
+  (let [unread? (not last-seen)]
+    [:div.repo-list_list-item.repo-list_list-item--hoverable
+     {:class    (if unread? "repo-list_list-item--unread" "")}
+     [row/row {:wrap false}
+      [col/col {:flex "30px"}
+       [repo-item-toolbar repo-full-name unread?]]
+      [col/col {:flex     "auto"
+                :on-click #(rf/dispatch [:releases/open-latest-notes repo-full-name])}
+       [repo-item-summary repo]]]]))
+
+(defn watched-repo-section []
   (let [repos @(rf/subscribe [:repos/watched-sorted])]
     [:section.section
      [:h2.section__title "Watching"]
      [:div.repo-list
-      (for [[full_name {:keys [description html_url language watchers tag_name published_at last-seen]
-                        :as   repo}] repos]
-        (let [unread? (not last-seen)]
-          ^{:key full_name}
-          [:div.repo-list_list-item.repo-list_list-item--hoverable 
-           {:class    (if unread? "repo-list_list-item--unread" "")}
-           [row/row {:wrap false}
-            [col/col {:flex "30px"}
-             [space/space {:direction :vertical :align :center}
-              [button/button {:shape    :circle
-                              :size     :small
-                              :on-click #(rf/dispatch [:repos/unwatch-repo full_name])}
-               "-"]
-              (if unread?
-                [notification-filled/notification-filled
-                 {:style    {:color :orange}
-                  :on-click #(rf/dispatch [:repos/mark-read full_name])}]
-                [notification-outlined/notification-outlined
-                 {:style    {:color :grey}
-                  :on-click #(rf/dispatch [:repos/mark-unread full_name])}])]]
-            [col/col {:flex     "auto"
-                      :on-click #(rf/dispatch [:releases/open-latest-notes full_name])}
-             [:a {:href html_url} full_name]
-             [:p.repo__description description]
-             [row/row {:class "repo__other-details"}
-              [col/col {:xs 12 :lg 6} "language: " language]
-              [col/col {:xs 12 :lg 6} "watchers: " watchers]
-              [col/col {:xs 12 :lg 6} "latest-release: " tag_name]
-              [col/col {:xs 12 :lg 6} "published: " published_at]]]]]))]]))
+      (for [[full_name repo] repos]
+        ^{:key full_name}
+        [watched-repo-item repo])]]))
 
 (defn testing-stuuf []
   [:div
@@ -128,7 +140,7 @@
     [:div.page
      [:h1.page__heading "Dashboard"]
      [repo-search-section]
-     [repo-watching-section]
+     [watched-repo-section]
      [release-details]
      [:br]
      [testing-stuuf]]]])
